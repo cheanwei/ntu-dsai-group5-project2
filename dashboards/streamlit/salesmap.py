a213feb4200917a41114f16a6d6c72bb2eb3f8f8
codeforlife888 <49854819+codeforlife888@users.noreply.github.com>
2026-09-13 14:09:53 +0800

    Add salesmap.py for visualizing revenues contributed by cities in map
    This script visualizes top revenue-generating cities in Brazil using Streamlit and Folium. It fetches city data from an API and displays it on a map.

---
+++ dashboards/streamlit/salesmap.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import streamlit as st
+import requests
+import folium
+from streamlit_folium import st_folium
+
+BASE = f"http://localhost:{5001}"
+
+# set_page_config must be the first Streamlit call on the page.
+st.set_page_config(page_title="Olist Brazilian E-Commerce", layout="wide")
+
+st.title(
+    ":material/compare_arrows: Top Revenue by Cities")
+st.markdown(
+    "Which cities generate the most revenue?")
+
+cities = pd.DataFrame(requests.get(f"{BASE}/api/cities").json().get("cities", []))
+if cities.empty:
+    st.warning("No data loaded.")
+    st.stop()
+st.dataframe(cities, width="stretch")
+
+
+m = folium.Map(location=(-23.545, -46.639), zoom_start=4, tiles="OpenStreetMap")
+
+# Brazil_cities = [{"name": "Brasília", "latitude":-15.8267, "longitude":-47.921},
+#                        {"name": "Rio de Janeiro", "latitude":-22.9068, "longitude":-43.1729}]
+for city in cities.to_dict(orient="records"):
+    folium.Marker(
+        location=[city["latitude"], city["longitude"]],
+        popup=city["name"],
+        icon=folium.Icon(color="blue")
+    ).add_to(m)
+
+st_data = st_folium(m, width=700, height=500)
+
+# Show click info
+#st.write("Map interaction:", st_data)
+
